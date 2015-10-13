3a992df442ae4aa3241c28f3d53cab2df638d9a7
Sandra <sandrachacon@alumnos.upm.es>
2015-10-13 20:06:40 +0200

    Actualizados

--- src/main/java/voting/presentation/presenters/ThemeManagerPresenter.java
+++ src/main/java/voting/presentation/presenters/ThemeManagerPresenter.java
@@ -7,12 +7,12 @@ import voting.presentation.models.Model;
 public class ThemeManagerPresenter {
 
 	public String process(Model model) {
-        model.put("log", "UI2Presenter:process");
+        model.put("log", "ThemeManagerPresenter:process");
         return "ThemeManagerView";
 	}
 
 	public String action1(Model model) {
-        model.put("log", "UI2Presenter:action1");
+        model.put("log", "ThemeManagerPresenter:action1");
         TransferObject2 transferObject2 = new BusinessController2().m1();
         model.put("transferObject2", transferObject2);
         return "ThemeManagerView";
--- src/main/java/voting/presentation/presenters/VotingPresenter.java
+++ src/main/java/voting/presentation/presenters/VotingPresenter.java
@@ -7,13 +7,13 @@ import voting.presentation.models.Model;
 public class VotingPresenter {
 
 	public String process(Model model) {
-        model.put("Temas", "");
+        model.put("Temas", "VotingPresenter:process");
         return "VotingView";
 	}
 	
 
 	public String action1(Model model) {
-        model.put("log", "UI2Presenter:action1");
+        model.put("log", "VotingPresenter:action1");
         TransferObject1 transferObject1 = new BusinessController1().m1();
         model.put("transferObject1", transferObject1);
         return "VotingView";
